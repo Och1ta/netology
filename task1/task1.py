f6455387c9f37383695991df1708796cacc3450e
Anton Kabanov <imcreativchik@gmail.com>
2023-03-22 15:34:55 +0500

    final commit

---
+++ task1/task1.py
@@ -0,0 +1,31 @@
+# Задание №1
+data = input("Введите дату: ")
+quest = input("Введите задачу: ")
+
+print(data, quest)
+
+# Задание №2
+d1 = input("Введите дату: ")
+q1 = input("Введите задачу: ")
+d2 = input("Введите дату: ")
+q2 = input("Введите задачу: ")
+d3 = input("Введите дату: ")
+q3 = input("Введите задачу: ")
+
+print(d1, q1, d2, q2, d3, q3)
+
+# Задание №3
+task_dict = {}
+
+date = input('Введите дату: ')
+task = input('Введите задачу: ')
+task_dict[date] = task
+
+date = input('Введите дату: ')
+task = input('Введите задачу: ')
+task_dict[date] = task
+
+
+date = input('Введите дату: ')
+task = input('Введите задачу: ')
+task_dict[date] = task
